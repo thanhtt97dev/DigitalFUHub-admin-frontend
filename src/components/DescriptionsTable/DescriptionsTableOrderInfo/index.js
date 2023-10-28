14506bb93bdcd70192731b423b81249a7c662e77
hiuhihi78 <leduchieu2001x@gmail.com>
2023-10-28 12:59:27 +0700

    fix order detail

--- src/components/DescriptionsTable/DescriptionsTableOrderInfo/index.js
+++ src/components/DescriptionsTable/DescriptionsTableOrderInfo/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Descriptions, Space, Typography, Rate, Button, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { Descriptions, Space, Typography, Tag, Button, Avatar, Row, Col } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments } from '@fortawesome/free-regular-svg-icons'
+
+import ModalChangeOrderStatus from '~/components/Modals/ModalChangeOrderStatus'
 
 import { formatStringToCurrencyVND, ParseDateTime } from '~/utils/index'
-import { Link } from 'react-router-dom';
-
+import {
+    ORDER_WAIT_CONFIRMATION,
+    ORDER_CONFIRMED,
+    ORDER_COMPLAINT,
+    ORDER_SELLER_REFUNDED,
+    ORDER_DISPUTE,
+    ORDER_REJECT_COMPLAINT,
+    ORDER_SELLER_VIOLATES
+} from '~/constants'
 
 const { Text, Title } = Typography;
 
-function DescriptionsTableOrderInfo({ order }) {
+function DescriptionsTableOrderInfo({ order, callBack }) {
     const items = [
         {
             key: '1',
@@ -79,7 +91,52 @@ function DescriptionsTableOrderInfo({ order }) {
         {
             key: '11',
             label: <h3>Trạng thái đơn hàng</h3>,
-            children: <></>,
+            children: <>
+                {(() => {
+                    if (order.orderStatusId === ORDER_WAIT_CONFIRMATION) {
+                        return <Tag color="#108ee9">Chờ xác nhận</Tag>
+                    } else if (order.orderStatusId === ORDER_CONFIRMED) {
+                        return <Tag color="#87d068">Đã xác nhận</Tag>
+                    } else if (order.orderStatusId === ORDER_COMPLAINT) {
+                        return <Tag color="#c6e329">Khiếu nại</Tag>
+                    } else if (order.orderStatusId === ORDER_SELLER_REFUNDED) {
+                        return <Tag color="#eab0b0e0">Người bán hoàn tiền</Tag>
+                    } else if (order.orderStatusId === ORDER_DISPUTE) {
+                        return (
+                            <Space>
+                                <Tag color="#ffaa01">Tranh chấp</Tag>
+                                <Button >Nhắn tin</Button>
+                                <ModalChangeOrderStatus orderId={order.orderId} callBack={callBack} />
+                            </Space>
+                        )
+                    } else {
+                        return (
+                            <Space direction='vertical'>
+                                <Space>
+                                    {(() => {
+                                        if (order.orderStatusId === ORDER_REJECT_COMPLAINT) {
+                                            return <Tag color="#ca01ff">Từ chối khiếu nại</Tag>
+                                        } else if (order.orderStatusId === ORDER_SELLER_VIOLATES) {
+                                            return <Tag color="#f50">Người bán vi phạm</Tag>
+                                        }
+                                    })()}
+                                    <Button type='primary'>
+                                        <FontAwesomeIcon icon={faComments} />
+                                        <span style={{ marginLeft: "10px" }}>Liên hệ với người mua và người bán</span>
+                                    </Button>
+                                </Space>
+
+                                <Row>
+                                    <Col >Nguyên nhân:</Col>
+                                    <Col offset={1} >
+                                        <span style={{ wordWrap: "break-word" }}>{order.note}</span>
+                                    </Col>
+                                </Row>
+                            </Space>
+                        )
+                    }
+                })()}
+            </>,
             span: 3
         },
 
