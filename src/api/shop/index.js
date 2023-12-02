70a0f3540c6b93baab8256fe8b6daded7592f7bf
hiuhihi78 <leduchieu2001x@gmail.com>
2023-12-02 14:50:38 +0700

    add shop detail

--- src/api/shop/index.js
+++ src/api/shop/index.js
@@ -1,5 +1,10 @@
-import { apiPostAuth } from '../defaultApi';
+import { apiPostAuth, apiGetAuth } from '../defaultApi';
 
 export const getShops = (data) => {
     return apiPostAuth(`api/shops/admin/all`, data);
 };
+
+export const getShopDetail = (id) => {
+    return apiGetAuth(`api/shops/GetDetail/${id}`);
+};
+
--- src/pages/Admin/ManageShop/ShopDetail/index.js
+++ src/pages/Admin/ManageShop/ShopDetail/index.js
@@ -1,7 +1,41 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+
+import { NotificationContext } from "~/context/UI/NotificationContext";
+
+import {
+    getShopDetail
+} from '~/api/shop'
+import Spinning from "~/components/Spinning";
+
 function ShopDetail() {
+
+    const { id } = useParams();
+    const notification = useContext(NotificationContext);
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        getShopDetail(id)
+            .then((res) => {
+
+            })
+            .catch(() => {
+                notification('error', "Hệ thống lỗi! Thử lại sau!");
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    setLoading(false)
+                }, 500)
+            })
+
+    }, [])
+
     return (
-        <>
-        </>
+        <Spinning spinning={loading}>
+
+        </Spinning>
     );
 }
 
