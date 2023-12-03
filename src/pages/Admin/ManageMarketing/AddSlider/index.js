c7b748b18e5b830332588e591f5a03e418a37449
hieunv <nguyenhieuskynett@gmail.com>
2023-12-03 10:30:47 +0700

    update code

--- src/pages/Admin/ManageMarketing/AddSlider/index.js
+++ src/pages/Admin/ManageMarketing/AddSlider/index.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import validator from 'validator';
 import classNames from 'classnames/bind';
 import styles from './AddSlider.module.scss';
-import { addSlider } from "~/api/slider";
 import Spinning from "~/components/Spinning";
+import { addSlider } from "~/api/slider";
 import { useAuthUser } from 'react-auth-kit';
 import { useNavigate } from 'react-router-dom';
-import { UserOutlined, PlusOutlined, QuestionCircleOutlined } from '@ant-design/icons';
-import { RESPONSE_CODE_SUCCESS, RESPONSE_CODE_NOT_ACCEPT, RESPONSE_CODE_DATA_NOT_FOUND } from '~/constants';
-import { NotificationContext } from "~/context/UI/NotificationContext";
-import { Col, Row, Form, Input, Button, Upload, Card, Tooltip, Modal, Switch } from 'antd';
 import { UPLOAD_FILE_SIZE_LIMIT } from '~/constants';
+import { NotificationContext } from "~/context/UI/NotificationContext";
+import { PlusOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import { RESPONSE_CODE_SUCCESS, RESPONSE_CODE_NOT_ACCEPT, RESPONSE_CODE_DATA_NOT_FOUND } from '~/constants';
+import { Col, Row, Form, Input, Button, Upload, Card, Tooltip, Modal, Switch } from 'antd';
 
+///
+const cx = classNames.bind(styles);
+///
 
 /// styles
 const styleCardAddSlider = { width: '100%' }
@@ -22,7 +25,6 @@ const AddSlider = () => {
     /// states
     const navigate = useNavigate();
     const [isLoadingSpinning, setIsLoadingSpinning] = useState(false);
-    const [isLoadingButtonAdd, setIsLoadingButtonAdd] = useState(false);
     const [previewImage, setPreviewImage] = useState('');
     const [sliderFile, setSliderFile] = useState([]);
     const [openNotificationFileExceedLimit, setOpenNotificationFileExceedLimit] = useState(false);
@@ -52,7 +54,7 @@ const AddSlider = () => {
         });
 
     const onFinish = (values) => {
-        debugger
+
         if (user === undefined || user === null) return navigate('/login');
 
         setIsLoadingSpinning(true);
@@ -74,6 +76,7 @@ const AddSlider = () => {
                     if (data.status.responseCode === RESPONSE_CODE_SUCCESS) {
                         notification("success", "Thêm mới slider thành công");
                         setIsLoadingSpinning(false);
+                        navigate('/admin/marketing/sliders');
                     } else if (
                         data.status.responseCode === RESPONSE_CODE_NOT_ACCEPT
                         ||
@@ -110,6 +113,8 @@ const AddSlider = () => {
         });
         setSliderFile(newFileList);
     };
+
+    const handleCancel = () => setPreviewOpen(false);
 
     const handlePreviewImage = async (file) => {
         if (!file.url && !file.preview) {
@@ -161,86 +166,95 @@ const AddSlider = () => {
                 </Row>
             </div>
         </Modal>
-        <Card title="Thông tin Slider" style={styleCardAddSlider} type="inner">
-            <Row>
-                <Form
-                    labelCol={{
-                        flex: '110px',
-                    }}
-                    name="control-hooks"
-                    form={form}
-                    onFinish={onFinish}
-                    labelWrap >
-                    <Form.Item name='name' label="Tên Slider" labelAlign="left" style={{ width: '100%' }}
-                        rules={[
-                            {
-                                validator: nameSliderValidator
-                            }
-                        ]}
-                    >
-                        <Row gutter={8}>
-                            <Col span={17}>
-                                <Input style={{ width: '100%' }} />
-                            </Col>
-                        </Row>
-
-                    </Form.Item>
-
-                    <Form.Item name='imageSlider' label={<lable style={{ fontSize: 14 }}>Ảnh slider (Hình ảnh tỷ lệ 1:3) <Tooltip title="Ảnh đại diện sản phẩm."><QuestionCircleOutlined /></Tooltip></lable>}
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Ảnh slider không để trống.'
-                            }
-                        ]}
-                    >
-                        <Upload
-                            beforeUpload={false}
-                            listType="picture-card"
-                            fileList={sliderFile}
-                            onPreview={handlePreviewImage}
-                            onChange={handleSliderChange}
-                            maxCount={1}
-                            accept=".png, .jpeg, .jpg"
-                        >
-                            {sliderFile.length < 1 ? <div>
-                                <PlusOutlined />
-                                <div
-                                    style={{
-                                        marginTop: 8,
-                                    }}
-                                >
-                                    Tải lên
-                                </div>
-                            </div> : null}
-                        </Upload>
-                    </Form.Item>
-
-                    <Form.Item name='productId' label="Id sản phẩm liên quan" labelAlign="left" style={{ width: '100%' }}>
-                        <Row gutter={8}>
-                            <Col span={17}>
-                                <Input style={{ width: '100%' }} />
-                            </Col>
-                        </Row>
-                    </Form.Item>
-                    <Row gutter={8}>
-                        <Col span={17}>
-                            <Form.Item name="isActive" label="Trạng thái" labelAlign="left" style={{ width: '100%' }}>
-                                <Switch checkedChildren="Hiển thị" unCheckedChildren="Ẩn" />
-                            </Form.Item>
-                        </Col>
+        <Modal open={previewOpen} title={previewImageTitle} footer={null} onCancel={handleCancel}>
+            <img
+                alt="thumbnail"
+                style={{
+                    width: '100%',
+                }}
+                src={previewImage}
+            />
+        </Modal >
+        <Card title="Thêm Slider" style={styleCardAddSlider} bodyStyle={{ padding: '20px 20%' }} type="inner">
+            <Form
+                layout="vertical"
+                name="control-hooks"
+                form={form}
+                onFinish={onFinish}
+                labelWrap >
+                <Form.Item name='name' label={<lable style={{ fontSize: 14 }}>Tên Slider <Tooltip title="Tên Slider được hiển thị trong phần quản lý"><QuestionCircleOutlined /></Tooltip></lable>} style={{ width: '100%' }}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Tên slider không được để trống'
+                        },
+                        {
+                            validator: nameSliderValidator
+                        }
+                    ]}
+                >
+                    <Row>
+                        <Input style={{ width: '100%' }} />
                     </Row>
 
-                    <Form.Item>
-                        <Row>
-                            <Col offset={4}>
-                                <Button type="primary" htmlType="submit" loading={isLoadingButtonAdd}>Thêm mới</Button>
-                            </Col>
-                        </Row>
-                    </Form.Item>
-                </Form>
+                </Form.Item>
 
-            </Row >
+                <Form.Item name='imageSlider' label={<label style={{ fontSize: 14 }}>Ảnh slider (Hình ảnh tỷ lệ 1:3) <Tooltip title="Ảnh được hiển thị tại trang chủ"><QuestionCircleOutlined /></Tooltip></label>}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Ảnh slider không để trống.'
+                        }
+                    ]}
+                >
+                    <Upload
+                        beforeUpload={false}
+                        listType="picture-card"
+                        fileList={sliderFile}
+                        onPreview={handlePreviewImage}
+                        onChange={handleSliderChange}
+                        maxCount={1}
+                        accept=".png, .jpeg, .jpg"
+                    >
+                        {sliderFile.length < 1 ? <div>
+                            <PlusOutlined />
+                            <div
+                                style={{
+                                    marginTop: 8,
+                                }}
+                            >
+                                Tải lên
+                            </div>
+                        </div> : null}
+                    </Upload>
+                </Form.Item>
+                <Form.Item name='productId'
+                    label={<lable style={{ fontSize: 14 }}>Mã sản phẩm liên quan <Tooltip title="Mã sản phẩm liên quan đến slider"><QuestionCircleOutlined /></Tooltip></lable>}
+                    style={{ width: '100%' }}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Mã sản phẩm liên quan không để trống.'
+                        }
+                    ]}>
+                    <Row>
+                        <Input style={{ width: '100%' }} />
+                    </Row>
+                </Form.Item>
+                <Row gutter={8}>
+                    <Col span={17}>
+                        <Form.Item name="isActive" label={<lable style={{ fontSize: 14 }}>Trạng thái <Tooltip title="Trạng thái hiển thị slider trên trang chủ"><QuestionCircleOutlined /></Tooltip></lable>} labelAlign="left" valuePropName="checked" style={{ width: '100%' }}>
+                            <Switch checkedChildren="Hiển thị" unCheckedChildren="Ẩn" />
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Form.Item>
+                    <Row className={cx('flex-item-center')}>
+                        <Button type="primary" htmlType="submit">Thêm mới</Button>
+                    </Row>
+                </Form.Item>
+            </Form>
         </Card >
     </Spinning>)
 }
