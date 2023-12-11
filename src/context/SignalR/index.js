7cfd16272bf6c90cc0aa26d5b4dfed9d117bff00
hiuhihi78 <leduchieu2001x@gmail.com>
2023-12-12 06:32:41 +0700

    fix signalR

--- src/context/SignalR/index.js
+++ src/context/SignalR/index.js
@@ -1,9 +1,12 @@
-import { Chat } from "~/context/SignalR/ChatContext";
+import React from "react";
 
+import { Chat } from "~/context/SignalR/ChatContext";
 import { UserOnlineStatus } from "~/context/SignalR/UserOnlineStatusContext";
 import { Notification as NotificationSignalR } from '~/context/SignalR/NotificationContext';
 
+
 function SignalR({ children }) {
+
     return (
         <>
             <NotificationSignalR>
