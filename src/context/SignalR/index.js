adb2f268af9201e07a85a1ebcb1a330c58bcb67b
hiuhihi78 <leduchieu2001x@gmail.com>
2023-12-12 09:54:40 +0700

    fix signalR

--- src/context/SignalR/index.js
+++ src/context/SignalR/index.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Chat } from "~/context/SignalR/ChatContext";
 import { UserOnlineStatus } from "~/context/SignalR/UserOnlineStatusContext";
 import { Notification as NotificationSignalR } from '~/context/SignalR/NotificationContext';
 
+import { URL_LOGIN_PAGE } from "~/constants/index"
+
 
 function SignalR({ children }) {
 
+    const [url, setUrl] = useState("");
+
+    setInterval(() => {
+        setUrl(window.location.pathname)
+    }, 10000)
+
     return (
         <>
-            <NotificationSignalR>
-                <UserOnlineStatus>
-                    <Chat>
-                        {children}
-                    </Chat>
-                </UserOnlineStatus>
-            </NotificationSignalR>
+            {url === URL_LOGIN_PAGE ?
+                <>{children}</>
+                :
+                <>
+                    <NotificationSignalR>
+                        <UserOnlineStatus>
+                            <Chat>
+                                {children}
+                            </Chat>
+                        </UserOnlineStatus>
+                    </NotificationSignalR>
+                </>
+            }
+
         </>
     );
 }
